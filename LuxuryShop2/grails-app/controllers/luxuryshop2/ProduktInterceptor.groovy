40adeb3783c5e97076137d60593b0bf5f2b65dce
Jacqueline Franßen <jacquelinefranssen@Jacquelines-MacBook-Pro.local>
2018-01-04 18:20:29 +0100

    added csv reader

--- LuxuryShop2/grails-app/controllers/luxuryshop2/ProduktInterceptor.groovy
+++ LuxuryShop2/grails-app/controllers/luxuryshop2/ProduktInterceptor.groovy
@@ -1,15 +1,52 @@
 package luxuryshop2
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 
 class ProduktInterceptor {
     ProduktService produktService
-        boolean gestartet = false
+    boolean gestartet = false
 
     boolean before() {
 
         if(!gestartet){
-            produktService.save(new  Produkt(produktname: "Test", preis: 10, waehrung: Waehrung.Euro, produktbeschreibung: ""))
-            println "loool "
+           /** produktService.save(new Produkt(produktname: "Test", preis: 10, waehrung: Waehrung.Euro, produktbeschreibung: ""))
+            println "loool "**/
+            String csvFile = "/Users/mkyong/csv/country.csv";
+            BufferedReader br = null;
+            String line = "";
+            String cvsSplitBy = ",";
+
+            try {
+
+                br = new BufferedReader(new FileReader(csvFile));
+                while ((line = br.readLine()) != null) {
+
+                    // use comma as separator
+                    String[] country = line.split(cvsSplitBy);
+
+                    System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
             gestartet= true
         }
 
