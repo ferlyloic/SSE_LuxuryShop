de316c7cba217d28d5471b30766314557d31a883
ferlyloic <ferlyloic@yahoo.fr>
2018-01-05 16:23:05 +0100

    add of return statement in before method

--- LuxuryShop2/grails-app/controllers/luxuryshop2/ProduktInterceptor.groovy
+++ LuxuryShop2/grails-app/controllers/luxuryshop2/ProduktInterceptor.groovy
@@ -14,7 +14,7 @@ class ProduktInterceptor {
     boolean gestartet = false
 
     boolean before() {
-
+    if(!gestartet){
         CSVReader reader = new CSVReader(new FileReader("grails-app/files/ProductList.csv"));
     //    CSVReader reader = new CSVReader(new FileReader("/Users/jacquelinefranssen/Desktop/SSE_LuxuryShop/SSE_LuxuryShop/LuxuryShop2/grails-app/files/ProductList.csv"), ',');
 
@@ -22,8 +22,7 @@ class ProduktInterceptor {
 
         // read line by line
         String[] record = null;
-        reader.readNext()
-
+        println reader.readNext()
         while ((record = reader.readNext()) != null) {
             println(Arrays.asList(record))
             Produkt produkt = new Produkt(produktname: record[0],preis: Double.parseDouble(record[1]), waehrung: Waehrung.Euro, produktbeschreibung:  record[2]);
@@ -32,6 +31,9 @@ class ProduktInterceptor {
         }
 
         reader.close();
+    }
+        gestartet = true
+        return true
     }
 
     boolean after() { true }
