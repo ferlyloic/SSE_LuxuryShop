733ded0d8a19c300d5b9085fd0e994a78e2625fc
ferlyloic <ferlyloic@yahoo.fr>
2018-01-06 15:58:22 +0100

    some changes

--- LuxuryShop2/grails-app/controllers/luxuryshop2/ProduktInterceptor.groovy
+++ LuxuryShop2/grails-app/controllers/luxuryshop2/ProduktInterceptor.groovy
@@ -14,23 +14,20 @@ class ProduktInterceptor {
     boolean gestartet = false
 
     boolean before() {
+        println(gestartet)
     if(!gestartet){
         CSVReader reader = new CSVReader(new FileReader("grails-app/files/ProductList.csv"));
-    //    CSVReader reader = new CSVReader(new FileReader("/Users/jacquelinefranssen/Desktop/SSE_LuxuryShop/SSE_LuxuryShop/LuxuryShop2/grails-app/files/ProductList.csv"), ',');
-
-        List<Produkt> produktList = new ArrayList<Produkt>();
-
         // read line by line
         String[] record = null;
         println reader.readNext()
         while ((record = reader.readNext()) != null) {
             println(Arrays.asList(record))
             Produkt produkt = new Produkt(produktname: record[0],preis: Double.parseDouble(record[1]), waehrung: Waehrung.Euro, produktbeschreibung:  record[2]);
-            println produkt
+            //println produkt
             produktService.save(produkt)
         }
 
-        reader.close();
+        reader.close()
     }
         gestartet = true
         return true
